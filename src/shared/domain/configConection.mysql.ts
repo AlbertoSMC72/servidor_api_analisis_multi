a804a348841d8c0be5bd95cc6873c8efe3af12c8
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-04-19 07:15:03 -0600

    modified:   src/shared/domain/configConection.mysql.ts

--- src/shared/domain/configConection.mysql.ts
+++ src/shared/domain/configConection.mysql.ts
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const configConnection: ConnectionOptions = {
-  host: process.env.HOST,
-  user: process.env.USER,
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 };
